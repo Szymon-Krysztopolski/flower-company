0cc15b1d9b608b9b5e7e81f91c025a36034600d6
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2023-12-29 17:09:20 +0100

    new endpoint

--- gateway/src/main/java/com/flowercompany/gateway/GatewayController.java
+++ gateway/src/main/java/com/flowercompany/gateway/GatewayController.java
@@ -3,6 +3,8 @@ package com.flowercompany.gateway;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1")
 public class GatewayController {
@@ -11,8 +13,13 @@ public class GatewayController {
         return ResponseEntity.ok("I got bouquet");
     }
 
-    @GetMapping("/receive/{id}")
-    public ResponseEntity<String> receive(@PathVariable String id) {
+    @GetMapping("/orders")
+    public ResponseEntity<List<String>> checkOrders() {
+        return ResponseEntity.ok(List.of("id1", "id2"));
+    }
+
+    @GetMapping("/status/{id}")
+    public ResponseEntity<String> status(@PathVariable String id) {
         return ResponseEntity.ok("example product with id: " + id);
     }
 }
