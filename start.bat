82c7c0b0a5ed35ba4d5fbc40d3f14cac62e06516
martai4 <tusiulka2011@gmail.com>
2024-01-14 21:22:14 +0100

    docker network

--- start.bat
+++ start.bat
@@ -1,5 +1,19 @@
 @REM @echo off 
-docker-compose up -d
-timeout /t 15
+docker compose down
+docker network rm shared-network
+docker network create shared-network --attachable --subnet 172.28.0.0/16
+docker image prune --all --force
+docker-compose up -d backend-factory backend-shop react-front
+
+:loop
+for /f %%i in ('curl --connect-timeout 2 -s -o NUL -w "%%{http_code}" http://localhost:8083/ws/status.wsdl') do set "status=%%i"
+if not "%status%"=="200" (
+    echo ..
+    timeout /t 3 /nobreak > NUL
+    goto :loop
+)
+
+echo Backend is up
 curl -f http://localhost:8083/ws/status.wsdl > gateway/status.wsdl
-docker-compose -f docker-compose2.yaml up -d
+
+docker-compose up -d backend-gateway
